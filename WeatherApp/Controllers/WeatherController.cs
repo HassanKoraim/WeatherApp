e96e4606dcb114a6a857b97374956c128b905feb
Hassan koraim <Hassankoraim2@gmail.com>
2024-04-09 19:38:47 +0200

    Complete the project almost

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // Hardcoded list of cities weather
+        List<CityWeather>? citiesWeather = new List<CityWeather>()
+        {
+            new CityWeather { CityUniqueCode = "LDN",
+            CityName = "London",
+            DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+            TemperatureFahrenheit = 33},
+            new CityWeather { CityUniqueCode = "NYC",
+            CityName = "London",
+            DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+            TemperatureFahrenheit = 60},
+            new CityWeather { CityUniqueCode = "PAR",
+            CityName = "Paris",
+            DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+            TemperatureFahrenheit = 82}
+        };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(citiesWeather);
+        }
+        [Route("/Weather/{cityCode}")]
+        public IActionResult CityDetails(string cityCode)
+        {
+            CityWeather? cityWeather = citiesWeather?.Where(city => city.CityUniqueCode == cityCode).FirstOrDefault();
+            if (cityWeather == null)
+            {
+                return Content("City not found");
+            }
+            return View(cityWeather);
+        }   
+    }
+}
--- WeatherApp/Models/CityWeather.cs
+++ WeatherApp/Models/CityWeather.cs
@@ -4,7 +4,7 @@
     {
         public string? CityUniqueCode { get; set;}
         public string? CityName { get; set;}
-        public DateTime? DateAndTime { get; set;}
-        public int? TemperatureFahrenheit { get; set;}
+        public DateTime DateAndTime { get; set;}
+        public int TemperatureFahrenheit { get; set;}
     }
 }
